e0a423a5b610fc43705f7a35ebbcf10e3cdd3884
yeyodiego <mattediego01@gmail.com>
2024-11-08 14:56:23 -0600

    Add project files.

---
+++ Sistema de Ventas/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sistema_de_Ventas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
